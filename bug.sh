afbd23ace3b09a2585b8bcd42cdd30704eb56b7d
anjaylu1 <76952646+anjaylu1@users.noreply.github.com>
2021-02-03 20:13:26 +0700

    Add files via upload

---
+++ bug.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+toilet -f big -F gay BY:DAFFA
+
+
+    echo "\033[34;1mSelamat Datang Di tools Kami"
+    echo "\033[34;1mSilakan Dipilih Menu nya :)"
+    echo "\033[32;1m===============•=============="
+    echo "\033[31;1m[00]EXIT"
+    echo "\033[31;1m[01]BUG TRI EDUKASI"
+    echo "\033[31;1m[02]BUG AXIS GAME"
+    echo "\033[31;1m[03]BUG AXIS EDUKASI(V2RAY)"
+    echo "\033[31;1m[04]BUG AXIS CONFERENCE"
+    echo "\033[31;1m[05]BUG AXIS VIDEO"
+    echo "\033[31;1m[06]BUG TSEL UNLIMAX"
+    echo "\033[31;1m[07]BUG TSEL UNLI CHAT"
+    echo "\033[31;1m[08]BUG TSEL KEMENDIKBUD"
+    echo "\033[31;1m[09]BUG TSEL MAXSTREAM"
+    echo "\033[31;1m[10]BUG TSEL KB-BIASA"
+    echo "\033[31;1m[11]BUG TSEL IG DAN YT"
+    echo "\033[31;1m[12]BUG IMCLASS (HTTP)"
+    echo "\033[31;1m[13]BUG IMCLASS (V2RAY)"
+    echo "\033[31;1m[14]BUG ISAT OPOK(V2RAY)"
+    echo "\033[31;1m[15]GRUP TELEGRAM"
+    echo "\033[32;1m===============•=============="
+  read -p "SILAKAN DIPILIH NGAB:)" pilih;
+  if [ $pilih = "01" ]
+  then
+    echo "CONNECT [host_port] [protocol]\r
+Host: classroom.google.com\r
+Connection: keep-alive\r
+Proxy-Connection: keep-alive\r
+X-Online-Host: classroom.google.com\r
+X-Forward-Host: classroom.google.com\r
+\r"
+     echo "METODE DIRECT (CUSTOM PAYLOAD)"
+     echo "NOTES: PAKAI PORT DROPBEAR"
+  elif [ $pilih = "02" ]
+  then
+     echo "www.freefiremobile.com(SNI)"
+    " www.pubgmobile.com(SNI)"
+     echo "NOTES: PAKEK PORT STUNNEL"
+  elif [ $pilih = "03" ]
+  then
+     echo "bimbel.ruangguru.com (NON TLS)"
+  elif [ $pilih = " 04" ]
+  then
+     echo "teams.microsoft.com (SNI)"
+  elif [ $pilih = " 05" ]
+  then
+     echo "Payload: CONNECT [host_port] [split]GET http://redirector.googlevideo.com/ HTTP/1.1[crlf]Connection: Keep-Alive[crlf][crlf]"
+     echo "Proxy : 202.152.240.50:3128"
+  elif [ $pilih = "06" ]
+  then
+     echo "v.snr.whatsapp.net (SNI)"
+     echo "www.instagram.com (SNI)"
+  elif [ $pilih = "07" ]
+  then
+     echo "v.snr whatsapp.net (SNI)"
+  elif [ $pilih = "08" ]
+  then
+     echo "www.whatsapp.net (SNI)"
+  elif [ $pilih = "09" ]
+  then
+     echo "www.hbogo.eu (SNI)"
+  elif [ $pilih = "10" ]
+  then
+     echo "Payload : CONNECT [host_port] HTTP/1.0[crlf]Host: Nope[crlf]X-Online-Host: Connection:Keep-Alive[crlf]Proxy-Authorization: Basic YWNoYmFyX3N5YWhwdXRyYUB5YWhvby5jb206MDMwNDk3[crlf][crlf]"
+     echo "Proxy : 202.4.186.76:3128"
+  elif [ $pilih = "11" ]
+  then
+     echo "www.instagram.com (SNI)"
+     echo " www.youtube.com (SNI)"
+  elif [ $pilih = "12" ]
+  then
+echo "Payload : CONNECT [host_port] [protocol]\r
+Host: www.udemy.com\r
+Connection: keep-alive\r
+Proxy-Connection: keep-alive\r
+X-Online-Host: www.udemy.com\r
+\r\n"
+     echo "Proxy : 124.158.167.172:8080"
+  elif [ $pilih = "13" ]
+  then
+     echo "www.ruangguru.com (NON TLS)"
+  elif [ $pilih = "14" ]
+  then
+     echo "covid19.go.id (NON TLS)"
+  elif [ $pilih = "15" ]
+  then
+     echo "t.me/tempatconfig"
+     echo "GABUNG YA BRO HEHE..."
+  elif [ $pilih = "00" ]
+  then
+     echo "TERIMAKASIH TELAH MENCOBA TOOLSNYA...!"
+     echo "exiting...!"
+     sleep 3
+     exit
+  else
+     echo "ERROR BOSQU: SILAKAN MASUKAN PERINTAH YANG SESUAI"
+     sleep 2
+  fi
